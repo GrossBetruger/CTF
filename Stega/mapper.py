2c3ece2bdfa6f7d67395556637ba4e353db83861
koi <koriatoren@gmail.com>
2017-05-21 00:22:03 +0300

    stega almost done

--- Stega/mapper.py
+++ Stega/mapper.py
@@ -1,22 +1,64 @@
 from PIL import Image
+import numpy as np 
+
 
 def load_image(name):
 	return Image.open(name)
 
+def plusminus1(num1, num2):
+	return num1 == (num2 + 1) or num1 == (num2 - 1) 
 
 def print_map(image_access, layout, search_mode=True):
+	last_access_sum = 0
 	for i in range(layout[0]):
 		for j in  range(layout[1]):
 			if search_mode:
-				if  image_access[i, j] != (255, 255, 255, 0):
+				if (plusminus1(sum(image_access[i, j]), last_access_sum)):
 					print image_access[i, j]
+					last_access_sum = sum(image_access[i, j])
 				else:
+					last_access_sum = sum(image_access[i, j])
 					continue
 
-			print image_access[i, j]
+def scan_y(image_access, y_size, x_val=0):
+	for y in range(y_size):
+		print image_access[x_val, y]
 
+
+
+def scan_x(image_access, x_size, y_const=0, every=False):
+	for x in range(x_size):
+		pixels = image_access[x, y_const]
+		if pixels not in [(255, 255, 255, 0)] or every:
+			print x,  ":", pixels, ","
+
+
+def scan_y_std(image_access, layout):
+	last_access_sum = 0
+	for y in  range(layout[1]):
+		x_0_values = []
+		x_1_values = []
+		x_2_values = []
+		for x in range(layout[0]):
+			x_0_values.append(image_access[x, y][0])
+			x_1_values.append(image_access[x, y][1])
+			x_2_values.append(image_access[x, y][2])
+		yield y, np.std(x_0_values) + np.std(x_1_values) + np.std(x_2_values)
+
+			# print image_access[i, j]
+"""
+according to std calculations y_dimension 25, in x values (293 - 399) contains the secret key
+which gives a range of 106 deformed pixels, closest factor of 8 is 112 (14 string keys * inner loop(8) = 112)
+"""
 if __name__ == "__main__":
 	secret = load_image("secret.png")
 	secret_access = secret.load()
-	print_map(secret_access, secret.size)
+	# scan_x(secret_access, secret.size[0]) # 16 rows of x different than default
+
+	scan_x(secret_access, secret.size[0], 25, every=1)
+	quit()
+	for i in scan_y_std(secret_access, secret.size):
+		if i[1] < 1 and i[1] > 0:
+			print i
+	# print_map(secret_access, secret.size)
 
--- Stega/steganography.py
+++ Stega/steganography.py
@@ -1,7 +1,18 @@
+# -*- coding: utf-8 -*-
 import argparse
 import math
 import random
 from PIL import Image
+from collections import defaultdict, OrderedDict
+import json
+import string
+
+DIRTY_X_VAL = 293
+
+DIRTY_Y_VAL = 25
+
+default_pixel = list((255, 255, 255, 0))
+FLAG = "flag{cant_you_see_hiding_in_plain_sight}"
 
 
 def do2(image_size, loaded_image, secret_string, rand1, rand2, x_factor):
@@ -9,23 +20,29 @@ def do2(image_size, loaded_image, secret_string, rand1, rand2, x_factor):
     counter = 0
     secret_string = list(secret_string)
     while len(secret_string) > 0:
+        print "iteration"
         secret_char = secret_string.pop(0)
         print "secret_char:", secret_char
         for b in range(8):
-            scrambled_secret_char = ((ord(secret_char) << b) & 0xFF) >> int((math.sqrt(math.sqrt(math.sqrt(16777217 - 1))) - 1))
+            print "inner loop"
+            scrambled_secret_char = ((ord(secret_char) << b) & 0xFF) >> 7 # zero or one (<=127. 0, >=128, 1)
             print "scrambled_secret_char:", scrambled_secret_char
-            scrambled_x_factor = ((x_factor * (2 ** b)) & 0xFF) >> int((0x1523486567 << 23 >> 54) / 7 + 1)
+            scrambled_x_factor = ((x_factor * (2 ** b)) & 0xFF) >> 7 # zero or one (<=127. 0, >=128, 1)
             print "scrambled_x_factor:", scrambled_x_factor
-            counter_devided_by_3 = int(counter / 3)
+            counter_devided_by_3 = int(counter / 3) # 0 0 0 1 1 1 2 2...
             print "counter_devided_by_3:", counter_devided_by_3
-            x_pointer = ((rand1 + counter_devided_by_3) % image_size[0])
+            x_pointer = ((rand1 + counter_devided_by_3) % image_size[0]) # starts 0, jumps 1 every 3
             print "x_pointer:", x_pointer
             y_pointer = (rand2 + int((counter_devided_by_3 + rand1) / image_size[0]))
             print "y_pointer:", y_pointer
             pixel_tuple = list(loaded_image[x_pointer, y_pointer])
+            print "before:", pixel_tuple[counter % 3]
+            before = pixel_tuple[counter % 3] # my line
             print "pixel_tuple:", pixel_tuple
-            pixel_tuple[counter % 3] = (pixel_tuple[counter % 3] | (scrambled_secret_char ^ scrambled_x_factor))
-            print "pixel_tuple[counter % 3] == ", pixel_tuple[counter % 3]
+            pixel_tuple[counter % 3] = (pixel_tuple[counter % 3] | (scrambled_secret_char ^ scrambled_x_factor)) # + 1 | + 0
+            print "pixel_tuple[counter % 3] == ", pixel_tuple[counter % 3], ",counter % 3 == ", counter % 3, "xor == ", scrambled_secret_char ^ scrambled_x_factor # counter%3, 0 1 2
+            print "after:", pixel_tuple[counter % 3]
+            print "same:", before == pixel_tuple[counter % 3]
             if pixel_tuple[counter % 3] % 2 and not scrambled_secret_char ^ scrambled_x_factor:
                 print "cond true!"
                 pixel_tuple[counter % 3] -= 1
@@ -37,18 +54,85 @@ def do2(image_size, loaded_image, secret_string, rand1, rand2, x_factor):
         x_factor &= 0xff
 
 
+def guess(vector, b_factor, x_factor, key_range_min=0x20, key_range_max=0x7e):
+    for key in [chr(i) for i in range(key_range_min, key_range_max)]:
+        predicate_vector = []
+        for b in range(b_factor, b_factor+3):
+            scrambled_x_factor = ((x_factor * (2 ** b)) & 0xFF) >> 7
+            scrambled_secret_char = ((ord(key) << b) & 0xFF) >> 7
+            predicate_vector.append(bool(scrambled_x_factor ^ scrambled_secret_char))
+
+        if vector == predicate_vector:
+            return key
+
+            # import string
+            # if key in string.printable:
+            #     return key
+
+
+def test_encoding(pixel_tuple, encoder):
+    for i in range(3):
+        if not encoder[i]:
+            pixel_tuple[i] = pixel_tuple[i] + 1
+    return pixel_tuple == default_pixel
+
+
+
+def rev_do2(image_size, loaded_image, flag, x_factor=97):
+
+    full_key = []
+    counter = 0
+    secret_string = list(flag)
+    while len(secret_string) > 0:
+        secret_char = secret_string.pop(0)
+        for b in range(8):
+            counter_devided_by_3 = int(counter / 3)  # 0 0 0 1 1 1 2 2...
+            x_offset = DIRTY_X_VAL
+            x_pointer = ((x_offset + counter_devided_by_3) % image_size[0]) # image_size[0] = 1116
+            y_pointer = DIRTY_Y_VAL
+            pixel_tuple_dirty = list(loaded_image[x_pointer, y_pointer])
+
+            if counter % 3  == 0:
+                encoder = encode_pixel(pixel_tuple_dirty)
+                lucky_guess = guess(encoder, b, x_factor, key_range_min=ord(secret_char), key_range_max=256)
+                full_key.append(lucky_guess)
+                assert test_encoding(pixel_tuple_dirty, encoder)
+
+            counter += 1
+        x_factor += 1
+        x_factor &= 0xff
+
+    raw = full_key
+    print raw
+    good = ""
+    indices = [2,5,7,10,13,15,18,21,23,26,29,31,34,37,39,42,45,47,50,53,55,58,61,63,66,69,71,74,77,79,82,85,87,90,93,95,98,101,103,106, 108]
+    for i in range(len(raw)):
+        # print indices[i], len(raw)
+        if indices[i] > len(raw):
+            break
+        if raw[indices[i]]:
+            good += raw[indices[i]]
+        else: good += raw[indices[i]-1]
+
+    return good
+
+
+
+
 def mb(*args):
     return [(args[int(i / 2)] >> ((i % 2) * 8)) & 0xff for i in range(len(args) * 2)]
 
 
 def do(image, secret_string):
     loaded_image = image.load()
-    rand1 = random.randint(0, (image.size[0] - int(len(secret_string) / image.size[0]) - 1) % 0xFFFF)
-    rand2 = random.randint(0, (image.size[1] - int(image.size[0] % len(secret_string)) - 1) % 0xFFFF)
+    print "image layout x:", image.size[0] # 1116
+    print "image layout y:", image.size[1] # 1143
+    rand1 = random.randint(0, (image.size[0] - int(len(secret_string) / image.size[0]) - 1) % 0xFFFF) # ~~ 0, 1116
+    rand2 = random.randint(0, (image.size[1] - int(image.size[0] % len(secret_string)) - 1) % 0xFFFF) # ~~ 0, 1143
     print "rand1:", rand1, "rand2:", rand2
     randomized_string = "".join([chr(j) for j in mb(rand1, rand2, len(secret_string) & 0xFFFF)])
     print "randomized string", randomized_string
-    do2(image.size, loaded_image, randomized_string, 0, 0, 0)
+    do2(image.size, loaded_image, randomized_string, 0, 0, 0) # NO RAND FACTORS
     NINETY_SEVEN = ((((165 & 44 * 2) << 5) ^ 26) ^ 41) + 46
     do2(image.size, loaded_image, secret_string, rand1, rand2, NINETY_SEVEN)
 
@@ -67,6 +151,48 @@ def main():
     do(image, args.string)
     image.save(args.out if args.out.endswith("png") else ".".join([args.out, "png"]))
 
+def load_changed():
+    with open("changed", "r") as f:
+        return OrderedDict(sorted(json.load(f).items(), key=lambda a: a[0]))
+
+def get_pixel(x_val):
+    found = load_changed().get(str(x_val))
+    return found or list((255, 255, 255, 0))
+
+
+def encode_pixel(pixel):
+    return [x != 254 for x in pixel[:3]]
+
+
+def create_flag(flag=""):
+    options = []
+    for c in [chr(i) for i in range(0, 256)]:
+
+        try:
+            temp = rev_do2(image.size, loaded, flag+c)
+            options.append((len(set(temp[-3:])), c))
+        except:
+            pass
+    choosen = list(sorted(options))[0][1]
+    flag += choosen
+    print flag
+    return create_flag(flag)
 
 if __name__ == "__main__":
+    image = Image.open('secret.png')
+    loaded = image.load()
+    # print rev_do2(image.size, loaded, flag=FLAG)
+    # quit()
+    print rev_do2(image.size, loaded, FLAG)
+    quit()
+
+    for c in [chr(i) for i in range(0, 256)]:
+        try:
+            raw = rev_do2(image.size, loaded, FLAG+c)
+            if raw:
+                print raw
+        except:
+            pass
+
+    quit()
     main()
